68f660e8ed3be8066ec08c0f70e05932b69a2987
Minh Thien <shondo3@Shondo3s-iMac.local>
2026-06-15 11:09:18 +0700

    fix host for render

--- server.py
+++ server.py
@@ -601,4 +601,5 @@ def download(job_id):
 if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
     print("🚀 PDF Translator Pro đang chạy tại http://localhost:5000")
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
