bc08631cb2587a9eb39f5379b96ac3e3547620ea
Minh Thien <shondo3@Shondo3s-iMac.local>
2026-06-15 11:28:54 +0700

    fix port 10000

--- server.py
+++ server.py
@@ -600,6 +600,6 @@ def download(job_id):
 
 if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
-    print("🚀 PDF Translator Pro đang chạy tại http://localhost:5000")
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
+    print(f"🚀 PDF Translator Pro đang chạy tại port {port}")
     app.run(debug=False, host="0.0.0.0", port=port)
